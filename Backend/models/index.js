58f05f5b7560162b77a2c1f7d861c9313b057c96
Rakib Ahsan <iamrakib242@gmail.com>
2024-01-27 01:20:44 +0600

    added complaint module

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,9 +1,11 @@
 // add models
 auth_model = require('./auth.model');
 dashboard_model = require('./dashboard.model');
+complaint_model = require('./complaint.model');
 
 module.exports = {
     // add models
     auth_model,
-    dashboard_model
+    dashboard_model,
+    complaint_model
 }
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,11 +1,14 @@
 // add routes
 const auth_router = require('./auth.route');
 const dashboard_router = require('./dashboard.route');
+const complaint_router = require('./complaint.route');
+
 
 const initiateRoutes = (router) => {
     // add routes
     auth_router(router);
     dashboard_router(router);
+    complaint_router(router);
 }
 
 module.exports = initiateRoutes;
