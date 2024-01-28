134229fd7099d0afdbba70430ca2ef4d4ebed7eb
Saany <80057431+al-aminSany@users.noreply.github.com>
2024-01-28 11:57:28 +0600

    added complaint frontend (partial)

---
+++ create-react-app/src/views/complaint/Progressbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Progressbar.css';
+
+const ProgressBar = () => {
+  const [progress, setProgress] = useState('pending');
+
+  const handleProgressChange = (newProgress) => {
+    setProgress(newProgress);
+  };
+
+  const renderCircle = (state) => {
+    return (
+      <div className="circle-container">
+        <button
+          className={`circle ${progress === state ? 'filled' : ''}`}
+          onClick={() => handleProgressChange(state)}
+          tabIndex={0}
+          key={state} // Add key for React's reconciliation
+        ></button>
+        <div className="state">{state}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="progress-bar-container">
+      
+      {renderCircle('pending')}
+      <div className="line"></div>
+      
+      {renderCircle('seen')}
+      <div className="line"></div>
+      
+      {renderCircle('processing')}
+      <div className="line"></div>
+      
+      {renderCircle('completed')}
+    </div>
+  );
+};
+
+export default ProgressBar;
